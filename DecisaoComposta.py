76c43082591a65e05ac9432a055e6ebe8b9ae056
Guilherme <guilhermeesm@gmail.com>
2022-11-16 16:12:13 -0300

    decision with IF-ELSE

---
+++ DecisaoComposta.py
@@ -0,0 +1,6 @@
+nome=input('Digite o nome: ')
+idade=int(input('digite a idade: '))
+if idade>=65:
+    print('O paciente '+nome+" POSSUI atendimento prioritario!")
+else:
+    print('O paciente '+nome+ ' NAO possui atendimento prioritario!')
