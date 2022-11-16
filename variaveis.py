ee0d84895d9bfd6169ce980e133b2747066ba8bf
Guilherme <guilhermeesm@gmail.com>
2022-11-16 15:44:52 -0300

    static var

---
+++ variaveis.py
@@ -0,0 +1,7 @@
+nome='Guilherme Seixas'
+empresa='FIAP'
+qtde_funcionarios=500
+mediaMensalidade=850.00
+print(nome + ' trabalha na empresa ' + empresa)
+print('Possui: ', qtde_funcionarios, ' funcionarios')
+print('A media da mensalidade e de: ' +str(mediaMensalidade))
