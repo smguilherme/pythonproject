844a040272c7ca3505e8649f4943d2c8a77312af
Guilherme <guilhermeesm@gmail.com>
2022-11-16 16:35:32 -0300

    decision with IF-ELSE-ELIF with AND

--- DecisaoComposta.py
+++ DecisaoComposta.py
@@ -1,9 +1,13 @@
 nome=input('Digite o nome: ')
 idade=int(input('digite a idade: '))
 doenca_infectocontagiosa=input('Suspeita de doenca infecto-contagiosa: ').upper()
-if idade>=65:
-    print('O paciente '+nome+" POSSUI atendimento prioritario!")
-elif doenca_infectocontagiosa=='SIM':
-    print('O paciente '+nome+ ' deve ser direcionado para a sala de espera reservada.')
+if idade >= 65 and doenca_infectocontagiosa == "SIM":
+    print("O paciente será direcionado para sala AMARELA - COM prioridade")
+elif idade < 65 and doenca_infectocontagiosa == "SIM":
+    print("O paciente será direcionado para sala AMARELA - SEM prioridade")
+elif idade >= 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente será direcionado para sala BRANCA - COM prioridade")
+elif idade < 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente será direcionado para sala BRANCA - SEM prioridade")
 else:
-    print('O paciente '+nome+ ' NAO possui atendimento prioritario e pode aguardar na sala comum!oGui')
+    print("Responda a suspeita de doença infectocontagiosa com SIM ou NAO")
